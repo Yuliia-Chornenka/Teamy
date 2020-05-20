863ae7b2edec9b7f0695a694cd81217d43180fb5
Vanchurick <davane4ek@gmail.com>
2020-05-20 12:54:48 +0300

    add routing to angular app, created components folder, created not-found-page and new-project-form component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { NewProjectFormComponent } from './components/new-project-form/new-project-form.component';
+
+
+const routes: Routes = [
+  { path: "create-project", component: NewProjectFormComponent },
+  {
+    path: "**",
+    component: NotFoundPageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NewProjectFormComponent } from './components/new-project-form/new-project-form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewProjectFormComponent
+    NewProjectFormComponent,
+    NotFoundPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
