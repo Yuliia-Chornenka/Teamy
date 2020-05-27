ed27188794e47b3be876ab5f6ed7adbb8105c90e
Vanchurick <davane4ek@gmail.com>
2020-05-27 10:57:28 +0300

    removed httpOptions in user.service

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { IUser } from '../Models/user.model';
-import {catchError, map} from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { User } from '../Models/user';
 
 
@@ -13,11 +13,6 @@ export class UserService {
 
   baseUrl = '/api/user';
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      Authorization: `Bearer ${window.localStorage.token}`,
-    })
-  };
 
   private handleError<T>(operation: string = 'operation', result?: T) {
     return (error: any): Observable<T> => {
@@ -31,20 +26,19 @@ export class UserService {
 
   addUser(user: User): Observable<User> {
     return this.http
-                .post<IUser>(`${this.baseUrl}/register`, user)
-                .pipe(
-      // map(res => {
-      // return user;
-      catchError(this.handleError('Register user', user))
-      // })
-    );
+      .post<IUser>(`${this.baseUrl}/register`, user)
+      .pipe(
+
+        catchError(this.handleError('Register user', user))
+
+      );
   }
 
   imageUpload(imageForm: FormData): Observable<any> {
-    return this.http.patch('/api/profile', imageForm, this.httpOptions);
+    return this.http.patch('/api/profile', imageForm);
   }
 
   getUserData(): Observable<IUser> {
-    return this.http.get('/api/profile', this.httpOptions);
+    return this.http.get('/api/profile');
   }
 }
