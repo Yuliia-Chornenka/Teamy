5bd3a57faf87c583fa17a3c509395f451a35ad81
Tatyana Dotsenko <tetiana_dotsenko@epam.com>
2020-05-28 13:30:29 +0300

    small fix on the call

--- src/app/Services/authentication.service.ts
+++ src/app/Services/authentication.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map } from 'rxjs/operators';
 import { Router } from '@angular/router';
-
 
 @Injectable({
   providedIn: 'root'
