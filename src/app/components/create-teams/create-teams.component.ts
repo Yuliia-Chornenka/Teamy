e1772f5f4067afded90f7d4687319aedc1cb7ad9
Yulia Chornenka <blackyulia583@gmail.com>
2020-06-08 15:22:49 +0300

    Add teams

--- src/app/components/create-teams/create-teams.component.ts
+++ src/app/components/create-teams/create-teams.component.ts
@@ -1,15 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {
-  AbstractControl,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem,
-} from '@angular/cdk/drag-drop';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Params } from '@angular/router';
@@ -25,12 +16,13 @@ import {
   LoadingStartAction,
   LoadingFinishAction,
 } from 'src/app/reducers/loading/loading.actions';
-import { AddMentorFormComponent } from '../project/add-mentor-form/add-mentor-form.component';
+import { AddMentorFormComponent, UserInterface } from '../project/add-mentor-form/add-mentor-form.component';
+import { ChatService } from '../../services/chat.service';
 
 @Component({
   selector: 'app-create-teams',
   templateUrl: './create-teams.component.html',
-  styleUrls: ['./create-teams.component.scss'],
+  styleUrls: [ './create-teams.component.scss' ],
 })
 export class CreateTeamsComponent implements OnInit {
   @Input() members: Array<object>;
@@ -40,6 +32,7 @@ export class CreateTeamsComponent implements OnInit {
   isPending = false;
   formCreateTeams: FormGroup;
   successCreation = true;
+  projectInfo: IProject;
 
   projectId: string;
   userId: string;
@@ -49,11 +42,13 @@ export class CreateTeamsComponent implements OnInit {
   constructor(
     public dialog: MatDialog,
     private projectService: ProjectService,
+    private chatService: ChatService,
     private snackBar: MatSnackBar,
     private store$: Store<LoadingState>,
     private userService: UserService,
     private route: ActivatedRoute
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -86,7 +81,7 @@ export class CreateTeamsComponent implements OnInit {
 
   openDialogNewMember() {
     this.dialog.open(AddMentorFormComponent, {
-      data: { projectId: this.projectId, membersMode: true },
+      data: {projectId: this.projectId, membersMode: true},
     });
   }
 
@@ -121,7 +116,7 @@ export class CreateTeamsComponent implements OnInit {
 
       for (let i = arrayOfStudents.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [arrayOfStudents[i], arrayOfStudents[j]] = [
+        [ arrayOfStudents[i], arrayOfStudents[j] ] = [
           arrayOfStudents[j],
           arrayOfStudents[i],
         ];
@@ -145,25 +140,143 @@ export class CreateTeamsComponent implements OnInit {
     }
   }
 
-  addTeams(): void {
-    console.log(this.randomTeams);
 
+  addTeams(): void {
     this.successCreation = true;
     this.store$.dispatch(new LoadingStartAction());
 
-    this.projectService.sendEmailToMembers().subscribe({
-      next: (response) => {
-        this.openSnackBar('Teams accepting and emails sent to everybody', '✔');
-      },
-      error: (err) => {
-        this.successCreation = false;
-        this.store$.dispatch(new LoadingFinishAction());
-      },
-      complete: () => {
-        this.store$.dispatch(new LoadingFinishAction());
-      },
+    let author = {};
+    this.userService.getUser(this.projectInfo.created_by).subscribe({
+        next: (projectCreator: IUser) => {
+          author = {
+            user_id: projectCreator._id,
+            user_name: projectCreator.name
+          };        },
+        error: (err) => {
+          this.successCreation = false;
+          this.store$.dispatch(new LoadingFinishAction());
+        },
+        complete: () => {
+          this.store$.dispatch(new LoadingFinishAction());
+
+          this.randomTeams.forEach((team, index: number) => {
+            const members = [];
+            team.forEach((member) => {
+              members.push({
+                user_id: member._id,
+                user_name: member.name
+              });
+            });
+
+            const mentors = [];
+            this.projectInfo.mentors.forEach((mentor: UserInterface) => {
+              mentors.push({
+                user_id: mentor._id,
+                user_name: mentor.name
+              });
+            });
+            mentors.push(author);
+
+            const teamItem = {
+              index,
+              team:
+                {
+                  projectId: this.projectInfo._id,
+                  projectName: this.projectInfo.title,
+                  members,
+                  mentors
+                }
+            };
+
+            this.chatService.createTeam(teamItem).subscribe({
+              next: (result) => {
+                console.log(result);
+              },
+              error: (err) => {
+                    this.successCreation = false;
+                    this.store$.dispatch(new LoadingFinishAction());
+              },
+              complete: () => {
+                this.store$.dispatch(new LoadingFinishAction());
+              },
+            });
+          });
+        },
     });
+
+    // this.projectService.sendEmailToMembers().subscribe({
+    //   next: (response) => {
+    //     this.openSnackBar('Teams accepting and emails sent to everybody', '✔');
+    //   },
+    //   error: (err) => {
+    //     this.successCreation = false;
+    //     this.store$.dispatch(new LoadingFinishAction());
+    //   },
+    //   complete: () => {
+    //     this.store$.dispatch(new LoadingFinishAction());
+    //   },
+    // });
   }
+
+
+
+
+  // addTeams(): void {
+  //   // console.log(this.randomTeams);
+  //   // console.log(this.projectInfo);
+  //   this.store$.dispatch(new LoadingStartAction());
+  //
+  //   this.randomTeams.forEach((team, index: number) => {
+  //     const teamItem = {
+  //       index,
+  //       team:
+  //           {
+  //             projectId: this.projectInfo._id,
+  //             projectName: this.projectInfo.title,
+  //             members: [
+  //               {
+  //                 user_id: team[index]._id,
+  //                 user_name: team[index].name
+  //               },
+  //             ],
+  //             mentors: this.projectInfo.mentors
+  //           }
+  //     };
+  //
+  //     console.log(teamItem);
+  //
+  //     // this.chatService.createTeam(teamItem).subscribe({
+  //     //   next: (result) => {
+  //     //     console.log(result);
+  //     //   },
+  //     //   error: (err) => {
+  //     //         this.successCreation = false;
+  //     //         this.store$.dispatch(new LoadingFinishAction());
+  //     //   },
+  //     //   complete: () => {
+  //     //     this.store$.dispatch(new LoadingFinishAction());
+  //     //   },
+  //     // });
+  //   });
+  //
+  //
+  //
+  //   // this.successCreation = true;
+  //   // this.store$.dispatch(new LoadingStartAction());
+  //   //
+  //   // this.projectService.sendEmailToMembers().subscribe({
+  //   //   next: (response) => {
+  //   //     this.openSnackBar('Teams accepting and emails sent to everybody', '✔');
+  //   //   },
+  //   //   error: (err) => {
+  //   //     this.successCreation = false;
+  //   //     this.store$.dispatch(new LoadingFinishAction());
+  //   //   },
+  //   //   complete: () => {
+  //   //     this.store$.dispatch(new LoadingFinishAction());
+  //   //   },
+  //   // });
+  // }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -202,6 +315,7 @@ export class CreateTeamsComponent implements OnInit {
   getProject(id): void {
     this.projectService.getProject(id).subscribe({
       next: (project: IProject) => {
+        this.projectInfo = project;
         this.projectAuthorId = project.created_by;
       },
       error: (err) => {
